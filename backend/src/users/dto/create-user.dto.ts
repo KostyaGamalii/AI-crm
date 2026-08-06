1fcccdaaab3c6e537d33b75e7ff744aeeb9847a4
Kostya.Gamaliy <kostya1.1gamaliy@gmail.com>
2026-08-06 11:28:28 +0200

    04 implement complete users CRUD with validation

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,4 +1,10 @@
+import {IsEmail, IsString, Length} from "class-validator";
+
 export class CreateUserDto {
+    @IsString()
+    @Length(2, 50)
     name: string;
+
+    @IsEmail()
     email: string;
 }
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,6 +1,8 @@
 import {Injectable} from '@nestjs/common';
 import {PrismaService} from "../prisma/prisma.service";
 import {CreateUserDto} from "./dto/create-user.dto";
+import {UpdateUserDto} from "./dto/update-user.dto";
+import { NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class UsersService {
@@ -22,5 +24,42 @@ export class UsersService {
         return this.prisma.user.create({
             data: createUserDto
         });
+    }
+
+    async update(userId: number, updateUserDto: UpdateUserDto) {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                id: userId
+            }
+        })
+
+        if (!user) {
+            throw new NotFoundException('User not found');
+        }
+
+        return this.prisma.user.update({
+            data: updateUserDto,
+            where: {
+                id: userId
+            }
+        });
+    }
+
+    async delete(userId: number) {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                id: userId
+            }
+        })
+
+        if (!user) {
+            throw new NotFoundException('User not found')
+        }
+
+        return this.prisma.user.delete({
+            where: {
+                id: userId
+            }
+        })
     }
 }
